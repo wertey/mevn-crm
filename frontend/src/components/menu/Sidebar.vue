781e0e90e73aaf44ea78489b8ccfe158d54ab090
Dmitry Verteyko <verteyko1990@gmail.com>
2020-06-07 21:19:31 +0300

    front

--- frontend/src/components/menu/Sidebar.vue
+++ frontend/src/components/menu/Sidebar.vue
@@ -19,15 +19,12 @@ export default {
   props: ['value'],
   data: () => ({
     links: [
-      {title: 'Счет', url: '/', exact: true},
-      {title: 'История', url: '/history'},
-      {title: 'Планирование', url: '/planning'},
-      {title: 'Новая запись', url: '/record'},
-      {title: 'Категории', url: '/categories'}
-    ]
-  })
-}
+      { title: 'Счет', url: '/', exact: true },
+      { title: 'История', url: '/history' },
+      { title: 'Планирование', url: '/planning' },
+      { title: 'Новая запись', url: '/record' },
+      { title: 'Категории', url: '/categories' },
+    ],
+  }),
+};
 </script>
-
-
-
