f3b1162d820a715444274bad222fa9ab9e63846c
Dmitry Verteyko <verteyko1990@gmail.com>
2020-06-07 22:46:20 +0300

    front

--- frontend/src/components/menu/NavBar.vue
+++ frontend/src/components/menu/NavBar.vue
@@ -5,7 +5,9 @@
         <a href="#" @click.prevent="$emit('click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">
+          {{ date }}
+        </span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -14,6 +16,7 @@
               class="dropdown-trigger black-text"
               href="#"
               data-target="dropdown"
+              ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +24,21 @@
 
           <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link
+                tag="a"
+                class="black-text"
+                :to="'/profile'"
+              >
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a
+                href="#"
+                class="black-text"
+                @click.prevent="logout"
+              >
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -37,3 +48,38 @@
     </div>
   </nav>
 </template>
+
+<script>
+import M from 'materialize-css';
+import moment from 'moment';
+
+export default {
+  name: 'Navbar',
+  data: () => ({
+    date: moment().format('MMMM Do YYYY, h:mm:ss a'),
+    interval: null,
+    dropdown: null,
+  }),
+  methods: {
+    logout() {
+      console.log('logout');
+      this.$router.push('/login?message=logout');
+    },
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = moment().locale('ru').format('MMMM Do YYYY, h:mm:ss');
+    }, 1000);
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: true,
+    });
+  },
+  beforeDestroy() {
+    console.log('before destroy');
+    clearInterval(this.interval);
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+  },
+};
+</script>
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -12,9 +12,13 @@
     </main>
 
     <div class="fixed-action-btn">
-      <a class="btn-floating btn-large blue" href="#">
+      <router-link
+        tag="a"
+        class="btn-floating btn-large blue"
+        :to="'/record'"
+      >
         <i class="large material-icons">add</i>
-      </a>
+      </router-link>
     </div>
   </div>
 </template>
